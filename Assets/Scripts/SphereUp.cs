74a74ca29aaf87ec5c0dc98c8d4b4bef39430573
Neil Sarkar <neil.r.sarkar@gmail.com>
2018-05-06 12:30:37 -0700

    moving sphere up

---
+++ Assets/Scripts/SphereUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello, sphere.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.up * .1f * Time.deltaTime;
+		
+	}
+}
