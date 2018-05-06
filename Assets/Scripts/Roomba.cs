886f14e76e6bb05449aefb8f5e6b8817f086e09c
eddie-git <edwin@consumerdirect.com>
2018-05-06 16:41:56 -0700

    Added roomba mesh and roomba script file

---
+++ Assets/Scripts/Roomba.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roombah : MonoBehaviour {
+
+    private Rigidbody roomba;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	private void Awake()
+	{
+        roomba = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        roomba.AddForce(Vector3.forward * 1000f * Time.deltaTime);
+		
+	}
+}
