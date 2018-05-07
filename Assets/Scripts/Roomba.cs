14ad304fa6373bbb2637139c6f04e1b48be2bd33
eddie-git <edwin@consumerdirect.com>
2018-05-06 19:54:45 -0700

    roomba incomplete mechanics

--- Assets/Scripts/Roomba.cs
+++ Assets/Scripts/Roomba.cs
@@ -2,24 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class roombah : MonoBehaviour {
+public class Roomba : MonoBehaviour {
 
     private Rigidbody roomba;
+    private bool rotate;
+    private bool move;
+    private float currentRads;
+    private int counter;
 
 	// Use this for initialization
 	void Start () {
-		
+        move = true;
+        rotate = false;
+        counter = 0;
 	}
 
-	private void Awake()
-	{
+	private void Awake() {
         roomba = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-
-        roomba.AddForce(Vector3.forward * 1000f * Time.deltaTime);
-		
+        if (rotate) {
+            RoombaRotate();
+        }
 	}
+
+	void FixedUpdate() {
+        if( move ) {
+            RoombaMove();     
+        }
+	}
+
+	void OnCollisionEnter(Collision collision) {
+        Debug.Log("hello");
+        move = false;
+        rotate = true;
+	}
+
+    void RoombaRotate() {
+        //roomba.AddForce(Vector3.forward * 0 * Time.deltaTime);
+        transform.Rotate(Vector3.forward * 60 * Time.deltaTime, Space.Self);
+    }
+
+    void RoombaMove() {
+        roomba.AddForce(Vector3.forward * 350f * Time.deltaTime);
+    }
 }
